fd39425a3976deb693f7e7598129e1f2714b1cb6
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-02-01 00:30:45 -0800

    Add initial styling and Landing Page

---
+++ client/src/pages/landing/Landing.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import main from '../../assets/images/main.svg';
+
+// styles
+import styles from './Landing.module.css';
+
+export default function landing() {
+  return (
+    <main>
+      <div className={styles.container}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Jobbr.</h1>
+          <p className={styles.subtitle}>The Job Application Tracker</p>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perferendis, voluptate, quis atque explicabo autem nam alias ipsum
+            ex ratione beatae reprehenderit? Iste numquam veritatis dolore quo
+            vel. Minus, animi inventore!
+          </p>
+          <Link to="/register" className={styles.btn}>
+            Login/Register
+          </Link>
+        </div>
+        <div className={`${styles.main_img} ${styles.img}`}>
+          <img src={main} alt="Jobbr" />
+        </div>
+      </div>
+    </main>
+  );
+}
