feceee939e7636201a8b30c668adbcf48ef2b887
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-02-01 16:28:33 -0800

    Add styles and fix classnames to Register form

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -13,25 +13,49 @@ export default function Register() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {memberStatus ? <h1>Login</h1> : <h1>Register</h1>}
-      {!memberStatus && <label htmlFor="name">Name</label>}
-      {!memberStatus && <input type="text" name="name" />}
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" />
-      <button>Submit</button>
-      {memberStatus ? (
-        <p>
-          Not signed up?<button onClick={toggleStatus}>Register</button>
-        </p>
-      ) : (
-        <p>
-          Already signed up?
-          <button onClick={toggleStatus}>Login</button>
-        </p>
-      )}
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <div className={styles.card}>
+        <span className={styles.logo}>AppTrak.</span>
+        {memberStatus ? <h1>Login</h1> : <h1>Register</h1>}
+        {!memberStatus && (
+          <label htmlFor="name" className={styles.form_label}>
+            Name
+          </label>
+        )}
+        {!memberStatus && (
+          <input type="text" name="name" className={styles.form_input} />
+        )}
+        <label htmlFor="email" className={styles.form_label}>
+          Email
+        </label>
+        <input type="text" name="email" className={styles.form_input} />
+        <label htmlFor="password" className={styles.form_label}>
+          Password
+        </label>
+        <input type="password" name="password" className={styles.form_input} />
+        <button className={`${styles.btn_submit} ${styles.btn}`}>Submit</button>
+        {memberStatus ? (
+          <p>
+            Not signed up?
+            <button
+              onClick={toggleStatus}
+              className={`${styles.btn_toggle} ${styles.btn}`}
+            >
+              Register
+            </button>
+          </p>
+        ) : (
+          <p>
+            Already signed up?
+            <button
+              onClick={toggleStatus}
+              className={`${styles.btn_toggle} ${styles.btn}`}
+            >
+              Login
+            </button>
+          </p>
+        )}
+      </div>
     </form>
   );
 }
