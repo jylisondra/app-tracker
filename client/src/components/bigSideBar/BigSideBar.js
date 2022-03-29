42af7c360d40d00a4e90bd349e102c9be6464cd7
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-03-29 00:51:31 -0700

    Center logo name

--- client/src/components/bigSideBar/BigSideBar.js
+++ client/src/components/bigSideBar/BigSideBar.js
@@ -1,3 +1,24 @@
+import classnames from 'classnames';
+import { useAppContext } from '../../context/AppContext';
+import styles from './BigSideBar.module.css';
+
 export default function BigSideBar() {
-  return <h1>BigSideBar</h1>;
+  const { showSidebar, toggleSidebar } = useAppContext();
+
+  return (
+    <div
+      className={classnames(
+        styles.big_sidebar,
+        showSidebar ? styles.show_sidebar : ''
+      )}
+    >
+      <div className={styles.content}>
+        <div className={styles.big_sidebar_header}>
+          <p>
+            <span>JOB</span>LY
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
