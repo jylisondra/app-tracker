d5518aa1cb482e150c202ee80b4994701235bf35
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-03-29 23:42:11 -0700

    Add NavLinks component

---
+++ client/src/components/navLinks/NavLinks.js
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import links from '../../utils/links';
+import { useAppContext } from '../../context/AppContext';
+import styles from './NavLinks.module.css';
+import classnames from 'classnames';
+export default function NavLinks() {
+  const { toggleSidebar } = useAppContext();
+  return (
+    <div className={styles.navlinks}>
+      {links.map((link) => {
+        const { id, text, path, icon } = link;
+        return (
+          <NavLink
+            key={id}
+            to={path}
+            className={({ isActive }) =>
+              classnames(styles.navlink, isActive ? styles.active : '')
+            }
+          >
+            {icon}
+            <span className={styles.link_name}>{text}</span>
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+}
---
+++ client/src/utils/links.js
@@ -0,0 +1,35 @@
+import {
+  RiDashboard3Line,
+  RiProfileLine,
+  RiBriefcase4Line,
+  RiFileUserLine,
+} from 'react-icons/ri';
+
+const links = [
+  {
+    id: 1,
+    text: 'Dashboard',
+    path: '/',
+    icon: <RiDashboard3Line />,
+  },
+  {
+    id: 2,
+    text: 'Jobs',
+    path: 'all-jobs',
+    icon: <RiProfileLine />,
+  },
+  {
+    id: 3,
+    text: 'Interviews',
+    path: 'interviews',
+    icon: <RiBriefcase4Line />,
+  },
+  {
+    id: 4,
+    text: 'Profile',
+    path: 'profile',
+    icon: <RiFileUserLine />,
+  },
+];
+
+export default links;
