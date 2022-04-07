d2aa4a9b7c585fb71b1703d538b63244081e549c
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-04-06 23:49:42 -0700

    Add Create Job button in sidebar

--- client/src/components/bigSideBar/BigSideBar.js
+++ client/src/components/bigSideBar/BigSideBar.js
@@ -1,10 +1,14 @@
-import NavLinks from '../navLinks/NavLinks';
+import { useState } from 'react';
 import { useAppContext } from '../../context/AppContext';
+import { Link } from 'react-router-dom';
+import NavLinks from '../navLinks/NavLinks';
 import classnames from 'classnames';
 import styles from './BigSideBar.module.css';
+import { HiPlus } from 'react-icons/hi';
 
 export default function BigSideBar() {
   const { showSidebar, toggleSidebar } = useAppContext();
+  const [showAddToggle, setAddToggle] = useState(false);
 
   return (
     <div
@@ -20,6 +24,12 @@ export default function BigSideBar() {
           </p>
         </div>
         <NavLinks />
+        <Link className={styles.add_link} to="/jobs/create">
+          <div className={styles.add_container}>
+            <HiPlus className={styles.add_icon} />
+            <span>Add Job</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
